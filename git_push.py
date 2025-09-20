4d8a8e817b72e286914a0155eb2931ae7f8ac1cc
tsugaide <arjunazarqa62@gmail.com>
2025-09-20 08:09:09 +0700

    commit otomatis

--- git_push.py
+++ git_push.py
@@ -7,17 +7,18 @@ for repo in repos.iterdir():
     if not repo.is_dir():
         continue
 
-    git_status = subprocess.run(["git", "status"], cwd=repo, capture_output=True, text=True)
+    git_status = subprocess.run(["git", "remote", "-v"], cwd=repo, capture_output=True, text=True)
 
-    if git_status.returncode != 0:
+    if "tsugaide" in git_status.stdout.strip():
+        print("ada")
+        subprocess.run(["git", "add", "."], cwd=repo)
+        subprocess.run(["git", "commit", "--allow-empty", "-m", "commit otomatis"], cwd=repo)
+        subprocess.run(["git", "push"], cwd=repo)
+    elif git_status.stdout == "":
+        print("ga ada")
         subprocess.run(["git", "init"], cwd=repo)
         subprocess.run(["git", "add", "."], cwd=repo)
         subprocess.run(["git", "commit", "--allow-empty", "-m", "commit otomatis"], cwd=repo)
         subprocess.run(["gh", "repo", "create", repo.name, "--public", "--source=.", "--remote=origin", "--push"], cwd=repo)
-    else:
- 
-        subprocess.run(["git", "add", "."], cwd=repo)
-        subprocess.run(["git", "commit", "--allow-empty", "-m", "commit otomatis"], cwd=repo)
-        subprocess.run(["git", "push"], cwd=repo)
-
+   
 print("selesai")
